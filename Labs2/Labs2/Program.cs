749ce051c04340a8d4304549d6f9fc1b6ac97c9c
danilsocol <danilsocol-2002@mail.ru>
2020-09-20 12:01:32 +0500

    start

---
+++ Labs2/Labs2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Labs2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
